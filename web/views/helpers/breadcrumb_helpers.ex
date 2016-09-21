70d253c473dffd64e7ea9c4bc80ce16591fd849a
Troy Nichols <troygnichols@gmail.com>
2016-09-21 00:47:31 -0500

    More style cleanup - add icons to breadcrumbs

--- web/views/helpers/breadcrumb_helpers.ex
+++ web/views/helpers/breadcrumb_helpers.ex
@@ -1,6 +1,19 @@
 defmodule Bookmarks.BreadcrumbHelpers do
 
   use Phoenix.HTML
+  alias Bookmarks.FontAwesomeHelpers, as: FA
+
+  def breadcrumb(name, icon: icon_name) do
+    content_tag :li do
+      FA.font_awesome_link name, icon_name, to: "javascript:void(0)", class: "breadcrumb-final"
+    end
+  end
+
+  def breadcrumb(name, target, icon: icon_name) do
+    content_tag :li do
+      FA.font_awesome_link name, icon_name, to: target
+    end
+  end
 
   def breadcrumbs(opts \\ [], do: contents) do
     content_tag :ul, contents, Keyword.merge(opts, id: "breadcrumb")
