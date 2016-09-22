1b210f3e7ccbd8fadfc92b67cd51f9abe5fb3df9
Troy Nichols <troygnichols@gmail.com>
2016-09-22 11:08:47 -0500

    format deps in mix.exs

--- mix.exs
+++ mix.exs
@@ -30,17 +30,18 @@ defmodule Bookmarks.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     # {:phoenix_html, "~> 2.6"},
-     {:phoenix_html, git: "https://github.com/troygnichols/phoenix_html.git", ref: "e195780", override: true},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.0"}
-   ]
+    [
+      {:phoenix, "~> 1.2.0"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      # {:phoenix_html, "~> 2.6"},
+      {:phoenix_html, git: "https://github.com/troygnichols/phoenix_html.git", ref: "e195780", override: true},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 2.0"},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
